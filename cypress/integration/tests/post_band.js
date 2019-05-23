79fe71aff704649e79e8f145566122565d0176c9
subhayum <smukherjee@adparlor.com>
2019-05-22 21:37:06 -0400

    added tests

---
+++ cypress/integration/tests/post_band.js
@@ -0,0 +1,26 @@
+describe('test the band post endpoint', function(){
+    it('tests creating a band', function(){
+        cy.server()
+        let a = {
+            method: "POST",
+            url: 'http://localhost:3000/bands',
+            body: {
+                "bandname": "Glass Onion",
+                "members": [
+                    {
+                        "name": "Subhayu",
+                        "instrument": "bass"
+                    },
+                    {
+                        "name": "Laddu",
+                        "instrument": "Guitars"
+                    }
+                ]
+            }
+        }
+        cy.request(a).then((response) =>{
+            expect(response.status).to.equal(200)
+            expect(response.body.bandname).to.equal('Glass Onion')
+        })
+    })
+})
